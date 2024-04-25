dedf1a2a2ef41504fcce647e18b171514fbeaa7f
Gourav Lohar <gouravlohar640@gmail.com>
2024-04-25 11:04:19 +0530

    updated

---
+++ Important_Python_Topics/OOPS/Built in  class function/main.py
@@ -0,0 +1,10 @@
+class Boy:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+e1=Boy("Tom",8)
+e2=Boy("Jerry",9)
+print(getattr(e1,"age"))
+setattr(e1,"name", "Rick")
+print(e1.__dict__)
+print(hasattr(e2,"Father"))
