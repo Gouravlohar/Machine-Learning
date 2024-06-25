4a02a7e9697d1367eafcdfe6bb6ef6eb4e82daac
Gourav Lohar <gouravlohar640@gmail.com>
2024-06-25 21:10:07 +0530

    Class Member

--- Important_Python_Topics/OOPS/2.Constructor/4.Class_Member.py
+++ Important_Python_Topics/OOPS/2.Constructor/4.Class_Member.py
@@ -6,6 +6,9 @@ class Employee:
         print(f"Employee Salary is {self.salary} and Age is {self.age}")
 e1=Employee(2000,20)
 e2=Employee(3500,40)
-
+#Accessing Class
+print(e1.salary)
+print(e2.age)
+#Accesing Methods
 e1.display()
 e2.display()
